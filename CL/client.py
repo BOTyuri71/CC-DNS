ff4235a857e5cb21208a34a7cbfa6edf493c984e
BOTyuri71 <gui.araujo5656@gmail.com>
2022-12-01 23:58:17 +0000

    update_client

--- CL/client.py
+++ CL/client.py
@@ -15,12 +15,28 @@ class Client:
     recursive_mode = False
 
     ###################################################################
-    # DNS message variables
+    # DNS query variables
 
-    pdu = []
-    header = {}
-    data = {}
+    query_pdu = []
+    query_header = {}
+    query_data = {}
     query_info_fields = {}
+
+    qheader_str = ''
+    qdata_str = ''
+    qpdu_str = ''
+
+    ###################################################################
+    # DNS response variables
+
+    response_pdu = []
+    response_header = {}
+    response_data = {}
+    response_info_fields = {}
+
+    rheader_str = ''
+    rdata_str = ''
+    rpdu_str = ''
 
     ###################################################################
 
@@ -41,18 +57,18 @@ class Client:
         else:
             print('Formato incorreto')
 
-#################################################################
+    #################################################################
 
     def header_builder(self):
-        self.header.update({'MESSAGE_ID': random.randint(1, 65535)})
+        self.query_header.update({'MESSAGE_ID': random.randint(1, 65535)})
         if self.recursive_mode == True:
-            self.header.update({'FLAGS': 'Q+R'})
+            self.query_header.update({'FLAGS': 'Q+R'})
         elif self.recursive_mode == False:
-            self.header.update({'FLAGS': 'Q'})
-        self.header.update({'RESPONSE-CODE': 0})
-        self.header.update({'N-VALUES': 0})
-        self.header.update({'N-AUTHORITIES': 0})
-        self.header.update({'N-EXTRA-VALUES': 0})
+            self.query_header.update({'FLAGS': 'Q'})
+        self.query_header.update({'RESPONSE-CODE': 0})
+        self.query_header.update({'N-VALUES': 0})
+        self.query_header.update({'N-AUTHORITIES': 0})
+        self.query_header.update({'N-EXTRA-VALUES': 0})
 
     def query_info_builder(self):
         self.query_info_fields.update({'NAME': self.name})
@@ -60,20 +76,38 @@ class Client:
 
     def data_builder(self):
         self.query_info_builder()
-        self.data.update({'RESPONSE VALUES': {}})
-        self.data.update({'AUTHORITIES VALUES': {}})
-        self.data.update({'EXTRA VALUES': {}})
+        self.query_data.update({'RESPONSE VALUES': {}})
+        self.query_data.update({'AUTHORITIES VALUES': {}})
+        self.query_data.update({'EXTRA VALUES': {}})
 
     def pdu_builder(self):
         self.header_builder()
         self.data_builder()
 
-        self.pdu.append(self.header)
-        self.pdu.append(self.data)
-        print(self.pdu)
+        print(self.query_header)
+        print(self.query_info_fields)
+        print(self.query_data)
+
+    #################################################################
+
+    def pdu_to_string(self):
+
+        self.pdu_builder()
+
+        self.qheader_str = str(self.query_header['MESSAGE_ID']) + ',' + self.query_header['FLAGS'] + ',' + str(self.query_header['RESPONSE-CODE']) + \
+            ',' + str(self.query_header['N-VALUES']) + ',' + \
+            str(self.query_header['N-AUTHORITIES']) + ',' + \
+            str(self.query_header['N-EXTRA-VALUES'])
+
+        self.qdata_str = str(
+            self.query_info_fields['NAME'] + ',' + self.query_info_fields['TYPE OF VALUE'])
+
+        self.qpdu_str = self.qheader_str + ';' + self.qdata_str + ';'
+
+        print(self.qpdu_str)
 
 
 cl = Client()
 terminal = str(input())
 cl.terminal_parser(terminal)
-cl.pdu_builder()
+cl.pdu_to_string()
