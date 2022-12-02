6bfe0bb6cba5413106ab03042a75a896b5bfcc49
BOTyuri71 <gui.araujo5656@gmail.com>
2022-12-02 00:41:20 +0000

    client network

--- CL/cl_network.py
+++ CL/cl_network.py
@@ -5,44 +5,28 @@ cl = cl.Client()
 terminal = str(input())
 cl.terminal_parser(terminal)
 
-msgFromServer = str(cl.pdu_to_string())
+msgFromClient = str(cl.pdu_to_string())
 
 bytesToSend = str.encode(msgFromServer)
 
-localIP = cl.ip_destino[0]
-localPort = cl.ip_destino[1]
+localIP = '10.0.0.2'
+localPort = 53
 bufferSize = 1024
 
-# Create a datagram socket
 
-UDPServerSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+# Create a UDP socket at client side
 
-
-# Bind to address and ip
-
-UDPServerSocket.bind((localIP, localPort))
+UDPClientSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
 
 
-print("UDP server up and listening")
+# Send to server using created UDP socket
+
+UDPClientSocket.sendto(bytesToSend, localPort)
 
 
-# Listen for incoming datagrams
+msgFromServer = UDPClientSocket.recvfrom(bufferSize)
 
-while (True):
 
-    bytesAddressPair = UDPServerSocket.recvfrom(bufferSize)
+msg = "Message from Server {}".format(msgFromServer[0])
 
-    message = bytesAddressPair[0]
-
-    address = bytesAddressPair[1]
-
-    clientMsg = "Message from Client: {}".format(message)
-    print(message)
-    clientIP = "Client IP Address:{}".format(address)
-
-    print(clientMsg)
-    print(clientIP)
-
-    # Sending a reply to client
-
-    UDPServerSocket.sendto(bytesToSend, address)
+print(msg)
